85098760213fa2c4a0db85c30a037fc8026fead2
Nethra.R <nethraramasamy515@gmail.com>
2026-07-09 18:30:19 +0530

    Debug Railway DB variables

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
-
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
 const express = require("express");
 const cors = require("cors");
 const db = require("./db");
